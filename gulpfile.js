a5e80fbf95e0ca624b123ff4414c72d069b2285b
codwerk <louis_widdy@live.fr>
2019-04-10 23:18:43 +0200

    Update gulpfile.js
    add gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
       sass = require('gulp-sass');
+      concat = require('gulp-concat');
 
 const paths = {
     css : {
@@ -12,6 +13,8 @@ function css(){
     return gulp.src(paths.css.src)
 
     .pipe(sass())
+
+    .pipe(concat('index.css'))
 
     .pipe(gulp.dest(paths.css.dest))
 }
